31587203a1302fa9186cf4fe598f2e979845e8f6
Sherry Martin <sm674@cornell.edu>
2019-01-04 14:31:27 +0000

    Automatically backed up by Learn

--- js-ajax-ajax-and-callbacks-readme-v-000/script.js
+++ js-ajax-ajax-and-callbacks-readme-v-000/script.js
@@ -2,3 +2,5 @@ $(document).ready(function() {
   $.get('this_doesnt_exist.html', function(data) {
     doSomethingGood();
 }).fail(function(error) {
+  console.log('Something went wrong: ' + error.statusText);
+});
