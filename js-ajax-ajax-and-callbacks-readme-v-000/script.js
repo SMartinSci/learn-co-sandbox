10a763052a1b7c53ef96b1c7e332ffe7723c1b74
Sherry Martin <sm674@cornell.edu>
2019-01-04 14:20:53 +0000

    Automatically backed up by Learn

--- js-ajax-ajax-and-callbacks-readme-v-000/script.js
+++ js-ajax-ajax-and-callbacks-readme-v-000/script.js
@@ -1,1 +1,7 @@
 $(document).ready(function() {
+  $.get('sentence.html', function(response) {
+        // Here we are getting the element on the page with the id of sentences and
+        // inserting the response
+        $('#sentences').html(response);
+    });
+});
